3427b1b5b3ac25686b6271ec82bc5517ca855d60
johnik <kotovjoni@gmail.com>
2025-02-26 16:43:07 +0300

    Add IRQ

---
+++ libs/platfotm/src/port.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * @brief  Checks whether the specified EXTI line is enabled or not.
+  * @param  EXTI_Line: specifies the EXTI line to check.
+  *   This parameter can be:
+  *     @arg EXTI_Linex: External interrupt line x where x(0..19)
+  * @retval The "enable" state of EXTI_Line (SET or RESET).
+  */
+ITStatus EXTI_GetITEnStatus(uint32_t EXTI_Line)
+{
+    ITStatus bitstatus = RESET;
+    uint32_t enablestatus = 0;
+    /* Check the parameters */
+    assert_param(IS_GET_EXTI_LINE(EXTI_Line));
+
+    enablestatus =  EXTI->IMR & EXTI_Line;
+    if (enablestatus != (uint32_t)RESET)
+    {
+        bitstatus = SET;
+    }
+    else
+    {
+        bitstatus = RESET;
+    }
+    return bitstatus;
+}
