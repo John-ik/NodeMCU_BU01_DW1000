783ec31c07e18c03782b64044ff39b8d12e09b6c
johnik <kotovjoni@gmail.com>
2025-04-17 00:03:29 +0300

    fix debug

--- Core/Inc/debug.h
+++ Core/Inc/debug.h
@@ -1,4 +1,5 @@
 #pragma once
+#ifdef DEBUG
 
 #include "usart.h"
 
@@ -9,13 +10,13 @@
 
 
 
-#ifdef DEBUG
-  static uint8_t DEBUG_uart_buf[512];
+extern char uart_buf[];
 
 #define DEBUG_transmit_fmt(fmt, ...) \
-  sprintf((char*)DEBUG_uart_buf, "%s:%d: " fmt "\n", __FILE__, __LINE__, __VA_ARGS__); HAL_UART_Transmit(&huart1, DEBUG_uart_buf, strlen((char*) DEBUG_uart_buf), 100)
+  sprintf(uart_buf, "%s:%d: " fmt "\n", __FILE__, __LINE__, __VA_ARGS__); HAL_UART_Transmit(&huart1, (uint8_t*)uart_buf, strlen(uart_buf), 100)
   
 #define DEBUG_transmit_str(str) DEBUG_transmit_fmt("%s", str)
-#endif
 
 #define DEBUG_transmit_b10(str, bytes) DEBUG_transmit_fmt(str " = 0x%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X", bytes[0],bytes[1],bytes[2],bytes[3],bytes[4],bytes[5],bytes[6],bytes[7],bytes[8],bytes[9])
+
+#endif
