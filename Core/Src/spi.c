9497f105253b903d021ff64b801b10c874049ee3
johnik <kotovjoni@gmail.com>
2025-06-10 23:15:36 +0300

    проверка

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -64,12 +64,16 @@ void MX_SPI1_Init(void)
   SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_LOW;
   SPI_InitStruct.ClockPhase = LL_SPI_PHASE_1EDGE;
   SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
-  SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV4;
+  SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV16;
   SPI_InitStruct.BitOrder = LL_SPI_MSB_FIRST;
   SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
-  SPI_InitStruct.CRCPoly = 10;
-  LL_SPI_Init(SPI1, &SPI_InitStruct);
+  SPI_InitStruct.CRCPoly = 7;
+  // LL_SPI_Init(SPI1, &SPI_InitStruct);
   /* USER CODE BEGIN SPI1_Init 2 */
+  __HAL_RCC_SPI1_CLK_ENABLE();
+  __HAL_RCC_SPI1_FORCE_RESET();
+  __HAL_RCC_SPI1_RELEASE_RESET();
+  LL_SPI_Init(SPI1, &SPI_InitStruct);
   LL_SPI_Enable(SPI1);
   /* USER CODE END SPI1_Init 2 */
 
